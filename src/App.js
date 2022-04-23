3a0ff1dd5643a775b48330a003d4d1264c835fa8
Kamil Kołatek <73605183+kamilkolatek@users.noreply.github.com>
2022-04-23 09:46:50 +0200

    added simple navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { useState } from 'react';
+
 import './App.css';
 
+const StartScreen = () => {
+  return <div>Start screen</div>
+}
+
+const GameScreen = () => {
+  return <div>Game screen</div>
+}
+
+const ResultScreen = () => {
+  return <div>Result screen</div>
+}
+
 function App() {
+  const [currentScreen, setCurrentScreen] = useState('start-screen');
+
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case 'start-screen':
+        return <StartScreen />
+      case 'game-screen':
+        return <GameScreen />
+      case 'result-screen':
+        return <ResultScreen />
+      default:
+        return <StartScreen />
+    }
+  }
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      {renderScreen()}
+    </div>
   );
 }
 
